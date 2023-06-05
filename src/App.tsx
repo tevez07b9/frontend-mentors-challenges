ce2d4e55dd411db6a5e61b3fc1ff3391a7279508
Talmeez Ahmed <talmeezahmed786@gmail.com>
2023-06-05 21:42:22 +0530

    fix homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import QrCode from "./routes/qrcode";
 import AgeCalculator from "./routes/agecalucator";
-import Homepage from "./routes/homepage";
 
 function App() {
   return (
     <Routes>
       <Route>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<AgeCalculator />} />
         <Route path="/qrcode" element={<QrCode />} />
         <Route path="/age-calculator" element={<AgeCalculator />} />
       </Route>
